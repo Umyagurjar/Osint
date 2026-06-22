b1a820b378269db8b79d1e44e922b491ea6d7b80
Umesh Gurjar <95861754+Umyagurjar@users.noreply.github.com>
2026-06-22 18:23:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import (
 )
 
 # --- CONFIGURATION ---
-TOKEN = os.getenv("8150517089:AAGgRxsAOg6-nGZkIqo4MVJuXhchXc4XFl8")
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 API_KEY = os.getenv("API_KEY", "Test")
 CHANNEL_ID = os.getenv("CHANNEL_ID", "@YOUR_CHANNEL_USERNAME")
 BASE_DOMAIN = "https://sbsakib.eu.cc/apis"
